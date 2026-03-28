e54f4a85e503c32a76d8893ae6c8ce202ca30720
Hani☠️ <hanija.lc240073@gmail.com>
2026-03-28 10:47:22 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ connectDB();
 
 // 1. Middlewares
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://localhost', 'http://localhost', process.env.FRONTEND_URL || ''].filter(Boolean),
+  origin: ['http://localhost:3000', 'https://localhost','https://karanfinance.com','http://localhost', process.env.FRONTEND_URL || ''].filter(Boolean),
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
