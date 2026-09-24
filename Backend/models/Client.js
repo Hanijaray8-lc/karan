6ff494d7c60436f2f6e3c9ef3902d0877a8ba404
Hanijaray8-lc <lcproduct566@gmail.com>
2026-09-24 16:11:08 +0530

    modal commit

--- Backend/models/Client.js
+++ Backend/models/Client.js
@@ -45,15 +45,6 @@ const clientSchema = new mongoose.Schema({
   district: {
     type: String,
     required: true,
-    enum: [
-      'Ariyalur','Chengalpattu','Chennai','Coimbatore','Cuddalore','Dharmapuri',
-      'Dindigul','Erode','Kallakurichi','Kanchipuram','Kanyakumari','Karur',
-      'Krishnagiri','Madurai','Mayiladuthurai','Nagapattinam','Namakkal',
-      'Nilgiris','Perambalur','Pudukkottai','Ramanathapuram','Ranipet',
-      'Salem','Sivaganga','Tenkasi','Thanjavur','Theni','Thoothukudi',
-      'Tiruchirappalli','Tirunelveli','Tirupathur','Tiruppur','Tiruvallur',
-      'Tiruvannamalai','Tiruvarur','Vellore','Viluppuram','Virudhunagar'
-    ]
   },
 
   amount: { type: Number, required: true, min: 0 },
